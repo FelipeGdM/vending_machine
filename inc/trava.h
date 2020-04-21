b9998b7b519ed2f958c4ba702f9a14e9dfe6dbc7
FelipeGdM <felipegmelo.42@gmail.com>
2020-04-21 16:17:21 -0300

    :sparkles: Add header da trava de manutenção

---
+++ inc/trava.h
@@ -0,0 +1,39 @@
+/**
+ * @file trava.h
+ * @author Felipe Gomes de Melo <felipegmelo@usp.br>
+ * @brief Métodos de interface com a trava de manutenção da vending machine
+ * @date 04/2020
+ *
+ * @copyright MIT License - Copyright (c) 2020
+ *
+ */
+
+#include <stdbool.h>
+
+#ifndef __TRAVA_H__
+#define __TRAVA_H__
+
+/**
+ * @brief Indica se a trava está aberta.
+ *
+ * Caso a trava seja aberta, a máquina irá para o estado de manutenção
+ * assim que passar pelo estado de início
+ *
+ */
+static bool trava_aberta;
+
+/**
+ * @brief Função de inicialização do componente
+ *
+ */
+void init_trava();
+
+/**
+ * @brief Retorna o estado da trava
+ *
+ * @return true Se a trava estiver aberta
+ * @return false Caso a trava esteja fechada
+ */
+bool trava_estado();
+
+#endif // __TRAVA_H__
