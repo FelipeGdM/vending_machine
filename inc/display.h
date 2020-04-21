52186dcd620c332656cdf0bbad8ccd927b3efa13
FelipeGdM <felipegmelo.42@gmail.com>
2020-04-21 16:19:33 -0300

    💡 Adiciona docs no código fonte

--- inc/display.h
+++ inc/display.h
@@ -3,15 +3,21 @@
  * @author Felipe Gomes de Melo <felipegmelo@usp.br>
  * @brief Métodos para controle da interface de texto da vending machine
  * @date 04/2020
- * 
+ *
  * @copyright MIT License - Copyright (c) 2020
- * 
+ *
  */
 
 #ifndef __DISPLAY_H__
 
 #include <stdint.h>
 
+/**
+ * @brief Exibe uma mensagem para o usuário por meio do display
+ *
+ * @param msg Mensagem a ser mostrada
+ * @param len Tamanho da mensagem
+ */
 void display_exibe_msg(char* msg, int8_t len);
 
 #endif // __DISPLAY_H__
--- inc/teclado.h
+++ inc/teclado.h
@@ -3,15 +3,27 @@
  * @author Felipe Gomes de Melo <felipegmelo@usp.br>
  * @brief Métodos para interação com teclado de entrada do usuário
  * @date 04/2020
- * 
+ *
  * @copyright MIT License - Copyright (c) 2020
- * 
+ *
  */
 
 #ifndef __TECLADO_H__
 #define __TECLADO_H__
 
+
+/**
+ * @brief
+ *
+ * @param buffer
+ */
 void teclado_le_entrada(char* buffer);
+
+/**
+ * @brief Lê o código de produto digitado pelo usuário
+ *
+ * @return uint8_t Código do produto digitado
+ */
 uint8_t teclado_le_prod_id();
 
 #endif // __TECLADO_H__
